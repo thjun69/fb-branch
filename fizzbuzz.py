456d1a0cd4f0c569abe3558629eec50c1f3c08a5
thjun69 <taeho.jun@gmail.com>
2025-08-22 12:21:22 +0900

    feat: 15 배수일 경우 fizzbuzz 출력

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -3,6 +3,8 @@ for i in range(1, 16+1):
         print('fizz')
     elif i%5 == 0:
         print('buzz')
+    elif i%15 == 0:
+        print('fizzbuzz')
     else:
         print(i)
 print("Test conflict")
